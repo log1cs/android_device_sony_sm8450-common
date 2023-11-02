9444fb39acda9019877adba38a09a45cadd37036
Luofan Chen <clfbbn@gmail.com>
2023-11-03 01:45:47 +0800

    sm8350-common: Move copy VNDK libs to common.mk
    Change-Id: Ifa8416f16a3a23890dda6623688decadb8d05252

--- common.mk
+++ common.mk
@@ -32,6 +32,10 @@ $(call inherit-product, vendor/sony/sm8350-common/sm8350-common-vendor.mk)
 # VNDK
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
