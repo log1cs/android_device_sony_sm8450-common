66ef92ade14b7533420e7bfbfc67e9dba8428788
Luofan Chen <clfbbn@gmail.com>
2024-02-15 22:44:03 +0800

    sm8350-common: vibrator: Read vibrator prop prefix on init
    Change-Id: Ieb7872beb83db557971a1dd27af12b68797772d7

--- aidl/vibrator/HardwareBase.cpp
+++ aidl/vibrator/HardwareBase.cpp
@@ -142,6 +142,16 @@ out:
 }
 
 HwCalBase::HwCalBase() {
+    // Get PROPERTY_PREFIX to be used later
+    auto propertyPrefix = std::getenv("PROPERTY_PREFIX");
+
+    if (propertyPrefix != NULL) {
+        mPropertyPrefix = std::string(propertyPrefix);
+    } else {
+        ALOGE("Failed get property prefix!");
+    }
+
+    // Read calibration from TA partition
     ALOGI("%s: Starting getting vibrator calibration data from TA partition", __func__);
     ta_handle = dlopen(LIB_MISCTA, RTLD_NOW);
     int ret;
