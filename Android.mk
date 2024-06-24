2a275c925081c6c632ca35850733027ea1955775
Luofan Chen <clfbbn@gmail.com>
2024-06-24 08:36:55 +0000

    sm8350-common: Declare wlan firmware files as symlinks during extraction
    Depends-On: Ie9b196e164d89eed16d20b8a9b68f2a6329d5605
    Change-Id: I75b2c6e50824e3b3a7b627c53d2786deeeff317b

--- Android.mk
+++ Android.mk
@@ -40,10 +40,6 @@ WIFI_FIRMWARE_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/
 $(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating wifi firmware symlinks: $@"
 	@mkdir -p $@/wlan/qca_cld
-	$(hide) ln -sf /vendor/etc/wifi/regdb.bin $@/regdb.bin
-	$(hide) ln -sf /vendor/etc/wifi/bdwlan.e47 $@/bdwlan.e47
-	$(hide) ln -sf /vendor/etc/wifi/bdwlan.e48 $@/bdwlan.e48
-	$(hide) ln -sf /vendor/etc/wifi/bdwlan.elf $@/bdwlan.elf
 	$(hide) ln -sf /data/vendor/firmware/wlanmdsp.mbn $@/wlanmdsp.otaupdate
 	$(hide) ln -sf /mnt/vendor/persist/wlan_mac.bin $@/wlan/qca_cld/wlan_mac.bin
 	$(hide) ln -sf /vendor/etc/wifi/WCNSS_qcom_cfg.ini $@/wlan/qca_cld/WCNSS_qcom_cfg.ini
