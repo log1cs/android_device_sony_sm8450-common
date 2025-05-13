216abe4c0edd8a1db40968421686935e717890ba
Bruno Martins <bgcngm@gmail.com>
2025-05-13 14:10:37 +0700

    sm8450-common: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- common.mk
+++ common.mk
@@ -188,6 +188,10 @@ PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/battchg_ext/smart_charging_interruption)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.sony
