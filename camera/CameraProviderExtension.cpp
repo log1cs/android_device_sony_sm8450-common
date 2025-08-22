15a6bc8a820456d931dcfd36fe35cc3b79eb7220
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-08-22 19:04:22 +0900

    sm8450-common: camera: Hardcode max brightness to 100
    Reason: LED completely died after seconds with maximum value 500
          (setting 500 == value on file after set is 257).
    
    Change-Id: Ib4aa991693ecad55d437771f582d12701716821f

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -48,10 +48,7 @@ int32_t getTorchDefaultStrengthLevelExt() {
 }
 
 int32_t getTorchMaxStrengthLevelExt() {
-    // In our device, both LEDs has same maximum value
-    // so get from one.
-    auto node = kTorchLedPath + "/" + TORCH_MAX_BRIGHTNESS;
-    return get(node, 0);
+    return 100;
 }
 
 int32_t getTorchStrengthLevelExt() {
