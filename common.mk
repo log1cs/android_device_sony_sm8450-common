886069b75ded5baa12a621f767d8ba108fa84c28
Tuan Anh <tuan73176@gmail.com>
2025-11-27 14:23:36 +0000

    Revert "sm6375-common: Build FM2"
    As for now, other than ODM products like the 10 series has FM hardware
    supports even on SEA/EU variants, 1/5 doesn't have one except Japanese
    variants and FM2 will eventually return hardware error.
    
    Workaround might be done later to enable FM but only for Japanese variant.
    
    This reverts commit bdc726796571212ced0a4ef86d1ab7cb0f84ac13.
    
    Change-Id: Id874d0d1a08385902e9654190065af1f80674e62

--- common.mk
+++ common.mk
@@ -147,10 +147,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2
-
 # GPS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
