80fe24d92528b1159c766780b487cf81f4366090
Arian <arian.kulmer@web.de>
2025-12-30 17:19:44 +0000

    sm8450-common: Copy powerhint.xml from power HAL
    Change-Id: I6f8ca59f4ad0ed7324010f657475d020fa76cb2f

--- common.mk
+++ common.mk
@@ -283,6 +283,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 $(call soong_config_set,qtipower,mode_ext_lib,//$(LOCAL_PATH):libpowermode-ext-sony)
 
 # PowerShare
