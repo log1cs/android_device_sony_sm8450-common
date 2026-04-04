f33288899159aaafa83bfcca2cf86d83ded9217a
Bruno Martins <bgcngm@gmail.com>
2026-04-04 06:32:29 +0000

    sm8450-common: Use common libqti-perfd-client and power-libperfmgr
    Change-Id: I22d6b35c72629553817005deaefcae60abc72cfd

--- common.mk
+++ common.mk
@@ -281,12 +281,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
-$(call soong_config_set,qtipower,mode_ext_lib,//$(LOCAL_PATH):libpowermode-ext-sony)
+$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libpowermode-ext-sony)
 
 # PowerShare
 PRODUCT_PACKAGES += \
@@ -328,6 +326,10 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/sony
 
 # Storage
